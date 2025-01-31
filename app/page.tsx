39cd126bb0598254b4db2daaba496c98a4311243
lokytech5 <lokytech5@gmail.com>
2025-01-31 16:17:45 +0000

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div>
       <h1 className="text-5xl font-bold">Static Web Page</h1>
       <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        Provident cupiditate et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. deleniti eaque aut repudiandae et a id nisi.
       </p>
       <button className="btn btn-primary">Get Started</button>
